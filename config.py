147b023a7aa51ee70f011aec16cb2f57c21e1eb6
Nishant467 <nishantkumar1092002@gmail.com>
2024-05-25 00:29:41 +0530

    Zenskar_Assignment Code

--- config.py
+++ config.py
@@ -4,4 +4,5 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     DATABASE = 'mydatabase.db'
-    STRIPE_API_KEY = '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'
+    STRIPE_API_KEY = ' Below is a stipe_API key'
+# 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
